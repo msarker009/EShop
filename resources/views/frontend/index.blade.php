7de9aef7f2826241f33375353c8cd86af8ece6c0
Mehedi <msmbd504@gmail.com>
2021-10-07 21:37:15 +0600

    add slider image done

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -6,5 +6,4 @@
 
 @section('content')
     @include('layouts.inc.slider')
-    <h1>Welcome</h1>
 @endsection
--- resources/views/layouts/inc/slider.blade.php
+++ resources/views/layouts/inc/slider.blade.php
@@ -1,6 +1,5 @@
-
-    <div class="container-fluid">
-        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+<div class="container-fluid">
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -8,13 +7,13 @@
             </div>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="{{asset('sliderImage/ss.jpg')}}" class="d-block w-100" alt="...">
+                    <img src="{{asset('sliderImage/ec.jpg')}}" class="img_size d-block w-100" alt="image">
                 </div>
                 <div class="carousel-item">
-                    <img src="{{asset('sliderImage/ss.jpg')}}" class="d-block w-100" alt="...">
+                    <img src="{{asset('sliderImage/best.jpg')}}" class="img_size d-block w-100" alt="image">
                 </div>
                 <div class="carousel-item">
-                    <img src="{{asset('sliderImage/ss.jpg')}}" class="d-block w-100" alt="...">
+                    <img src="{{asset('sliderImage/ms.jpg')}}" class="img_size d-block w-100" alt="image">
                 </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -26,6 +25,6 @@
                 <span class="visually-hidden">Next</span>
             </button>
         </div>
-    </div>
+</div>
 
 
